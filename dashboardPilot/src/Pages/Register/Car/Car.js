f74464fecd7f861a5013180b85487f64640a3f21
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-09-28 21:13:57 -0300

    Def para receber os dados feita, falta implementar no dropdown

--- dashboardPilot/src/Pages/Register/Car/Car.js
+++ dashboardPilot/src/Pages/Register/Car/Car.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+
+import {View,Text} from 'react-native';
 import InputBasic from '../../../Components/InputBasic';
 import {setCarData} from '../../../Store/register/actions';
 import {useDispatch} from 'react-redux';
 import {getData,getCarTest} from '../../../Functions/axios';
+import axios from 'axios';
 import DropDown from '../../../Components/Dropdown'
 import Icon from 'react-native-vector-icons/Feather';
 import colors from '../../../Themes/colors';
 
 export default function Car(props) {
-  const {getCarInfo, getTrackInfo} = getData();
+  //const {getCarInfo, getTrackInfo} = getData();
   //const {dispatch} = props;
   const dispatch = useDispatch();
   function setData(values, label) {
     dispatch(setCarData({[label]: values}));
   };
-
+  const data1 = [];
+  const [data, setRequestData] = useState([]);
+  //console.log("Printando data "+data);
+  async function getCaarInfo(){
+    await axios
+      .get('https://apirestmileage.herokuapp.com/api/car/')
+      .then(function (response){
+        //console.log(response.data);
+        //console.log("print da data")
+        response.data.map((data,key)=> {
+          //console.log(data.name);
+        });
+        setRequestData(response.data);
+        //console.log(response.data[0]);
+        //setRequestData(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+        Alert.alert('Deu errado no getCarInfo :(');
+      });
+    };
+    getCaarInfo();
   // getCarInfo().map(data =>{
   //   console.log(data.name);
   // });
