aef2634eedb98725dae67f845a8281071eea54ee
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-10-05 23:09:37 -0300

    dropdown coerente com o GET, falta criar uma page para atualizar

--- dashboardPilot/src/Pages/Register/Car/Car.js
+++ dashboardPilot/src/Pages/Register/Car/Car.js
@@ -4,7 +4,6 @@ import {View,Text} from 'react-native';
 import InputBasic from '../../../Components/InputBasic';
 import {setCarData} from '../../../Store/register/actions';
 import {useDispatch} from 'react-redux';
-import {getData,getCarTest} from '../../../Functions/axios';
 import axios from 'axios';
 import DropDown from '../../../Components/Dropdown'
 import Icon from 'react-native-vector-icons/Feather';
@@ -17,21 +16,15 @@ export default function Car(props) {
   function setData(values, label) {
     dispatch(setCarData({[label]: values}));
   };
-  const data1 = [];
   const [data, setRequestData] = useState([]);
   //console.log("Printando data "+data);
   async function getCaarInfo(){
     await axios
       .get('https://apirestmileage.herokuapp.com/api/car/')
       .then(function (response){
-        //console.log(response.data);
-        //console.log("print da data")
-        response.data.map((data,key)=> {
-          //console.log(data.name);
+        response.data.map((datae,key)=> {
+          setRequestData(data => [...data, {label: datae.name, value: "bla", icon: () => <Icon name="flag" size={18} color={colors.light_orange} />}])
         });
-        setRequestData(response.data);
-        //console.log(response.data[0]);
-        //setRequestData(response.data)
       })
       .catch(function (error) {
         console.log(error);
@@ -39,15 +32,6 @@ export default function Car(props) {
       });
     };
     getCaarInfo();
-  // getCarInfo().map(data =>{
-  //   console.log(data.name);
-  // });
-  
-  // let data=[
-  //   {label: 'Test', value: 'Test', icon: () => <Icon name="flag" size={18} color={colors.light_orange} />},
-  //   {label: 'Competition', value: 'Competition', icon: () => <Icon name="flag" size={18} color={colors.orange} />},
-  //   {label: 'Inspection', value: 'Inspection', icon: () => <Icon name="flag" size={18} color={colors.red} />},
-  // ];
   return (
     <View>
       <InputBasic
@@ -58,13 +42,12 @@ export default function Car(props) {
           setData(e, 'name');
         }}
       />
-      {/* <DropDown
+      {data.length >0 ? <DropDown
         label="Car"
         height={40}
         data = {data}
         onChangeItem={item => setData(item.value,'name')}
-        defaultValue={'Test'}
-      /> */}
+        defaultValue="bla" />: <View></View>}
       <InputBasic
         label="Description"
         height={120}
